bd18efc9efc73ae2f0d971bf90450227f2e6b473
Naresh Chole <nareshchole2004@gmail.com>
2026-06-07 15:34:50 +0530

    Add JWT authentication

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+
+const protect = async (req, res, next) => {
+  try {
+
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET
+      );
+
+      req.user = decoded;
+
+      next();
+
+    } else {
+
+      return res.status(401).json({
+        message: "Not Authorized, No Token",
+      });
+
+    }
+
+  } catch (error) {
+
+    return res.status(401).json({
+      message: "Not Authorized, Invalid Token",
+    });
+
+  }
+};
+
+module.exports = { protect };

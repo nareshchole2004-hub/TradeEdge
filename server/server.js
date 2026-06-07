9dd47e895a192653633e454f206f01cd7cf45418
Naresh Chole <nareshchole2004@gmail.com>
2026-06-07 13:57:05 +0530

    Completed authentication module

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("TradeEdge Backend Running");
